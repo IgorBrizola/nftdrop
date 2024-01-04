08e5bb18548a69f485259486a86f458ecbe8b505
IgorBrizola <igorbrizzola@gmail.com>
2024-01-04 12:12:22 -0300

    quarto commit

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,6 +4,8 @@ import {sanityClient, urlFor} from '../sanity'
 import {Collection} from '../typings'
 import Link from "next/link"
 
+import Image from 'next/image'
+
 interface Props{
   collections: Collection[]
 }
@@ -25,9 +27,9 @@ const Home = ({collections}: Props) =>{
       <main className="bg-slate-100 p-10 shadow-xl shadow-rose-400">
         <div className="grid space-x-3 md:grid-cols-2 lg:grid-cols-3 2xl:grid-cols-4">
           {collections.map(collection => (
-<Link href={`/nft/${collection.slug.current}`}>
+<Link href={`/nft/${collection.slug.current}`} key={collection._id}>
             <div className="flex cursor-pointer flex-col items-center transition-all duration-200 hover:scale-105">
-              <img className="h-96 w-60 rounded-2xl object-cover" src={urlFor(collection.mainImage).url()} alt="" />
+              <Image className="h-96 w-60 rounded-2xl object-cover" src={urlFor(collection.mainImage).url()} alt="" />
               <div className="p-5">
                 <h2 className="text-3xl">{collection.title}</h2>
                 <p className="mt-2 text-sm text-gray-400">{collection.description}</p>
--- pages/nft/[id].tsx
+++ pages/nft/[id].tsx
@@ -5,6 +5,7 @@ import { sanityClient, urlFor } from '@/sanity';
 import { Collection } from '@/typings';
 import Link from 'next/link';
 
+import Image from 'next/image'
 
 interface Props{
     collection: Collection
@@ -33,7 +34,7 @@ const NFTDropPage = ({collection}: Props) => {
             <div className='bg-gradient-to-br from-cyan-800 to-rose-500 lg:col-span-4'>
                 <div className='flex flex-col items-center justify-center py-2 lg:min-h-screen'>
                     <div className='rounded-xl bg-gradient-to-br from-yellow-400 to-purple-600 p-2'>
-                        <img className='w-44 rounded-xl object-cover lg:h-96 lg:w-72' src={urlFor(collection.previewImage).url()} alt="" />
+                        <Image className='w-44 rounded-xl object-cover lg:h-96 lg:w-72' src={urlFor(collection.previewImage).url()} alt="" />
                     </div>
 
                     <div className='text-center p-5 space-y-2'>
@@ -67,7 +68,7 @@ const NFTDropPage = ({collection}: Props) => {
                 {/* Content */}
 
                 <div className='mt-10 flex flex-1 flex-col items-center space-y-6 text-center lg:space-y-0 lg:justify-center'>
-                    <img className='w-80 object-cover pb-10 lg:h-40' src={urlFor(collection.mainImage).url()} alt="" />
+                    <Image className='w-80 object-cover pb-10 lg:h-40' src={urlFor(collection.mainImage).url()} alt="" />
 
                     <h1 className='text-3xl font-bold lg:text-5xl lg:font-extrabold'>{collection.title}</h1>
 
